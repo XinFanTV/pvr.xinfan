bbad0894277a0c10c1793d4b36e346bd6b743786
CosmoCommerce <dev@cosmocommerce.com>
2015-09-11 23:33:28 +0800

    initial

---
+++ CMakeFiles/addon-package.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/addon-package"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/addon-package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ CMakeFiles/pvr.xinfan.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pvr.xinfan.dir/src/client.cpp.o"
+  "CMakeFiles/pvr.xinfan.dir/src/PVRXinFanData.cpp.o"
+  "pvr.xinfan.pdb"
+  "pvr.xinfan.so"
+  "pvr.xinfan.so.1.10.7"
+  "pvr.xinfan.so.15.2"
+  "pvr.xinfan.pdb"
+  "CMakeFiles/CMakeRelink.dir/pvr.xinfan.so"
+  "CMakeFiles/CMakeRelink.dir/pvr.xinfan.so.1.10.7"
+  "CMakeFiles/CMakeRelink.dir/pvr.xinfan.so.15.2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pvr.xinfan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
